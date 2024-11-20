2d78555fb3bf9fea4e9ad58c85e69dbfdc1e6088
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-20 16:33:32 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -19,5 +19,34 @@ const RecipeList = () => {
     </div>
   );
 };
+import React, { useEffect } from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  // If there are no filtered recipes, show all recipes by default
+  useEffect(() => {
+    if (filteredRecipes.length === 0 && recipes.length > 0) {
+      useRecipeStore.getState().filterRecipes(); // Perform initial filtering
+    }
+  }, [recipes, filteredRecipes]);
+
+  return (
+    <div>
+      {filteredRecipes.length === 0
+        ? <p>No recipes found.</p>
+        : filteredRecipes.map((recipe) => (
+            <div key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </div>
+          ))
+      }
+    </div>
+  );
+};
 
 export default RecipeList;
+
