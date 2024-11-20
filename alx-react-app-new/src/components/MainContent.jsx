3a436937401fb6d10e92678078484f4bec52fa46
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-20 09:57:05 +0200

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent({ content }) {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f5f5f5', lineHeight: '1.6', fontSize: '1.1rem' }}>
-      {content}
+      {content}I love to visit New York, Paris, and Tokyo.
     </main>
   );
 }
