1bb1cde9959affb2bf4946ef918c1b5686458aee
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-20 15:56:31 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -166,24 +166,8 @@ function App() {
     </Router>
   );
 }
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails';
-import AddRecipeForm from './components/AddRecipeForm';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-          <Route path="/add-recipe" element={<AddRecipeForm />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+
 
 export default App;
 
