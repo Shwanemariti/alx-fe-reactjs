903e3f04529a3aa3d62f928c0246b2c68510b1b6
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-20 14:53:34 +0200

    Create DeleteRecipeButton.jsx

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,14 @@
+import { useRecipeStore } from './recipeStore';
+
+const DeleteRecipeButton = ({ id }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  const handleDelete = () => {
+    deleteRecipe(id);
+    alert('Recipe deleted!');
+  };
+
+  return <button onClick={handleDelete}>Delete Recipe</button>;
+};
+
+export default DeleteRecipeButton;
