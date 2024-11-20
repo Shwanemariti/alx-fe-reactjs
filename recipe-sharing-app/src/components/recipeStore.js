fab7f3dcc705126f926faf5b0baa820eb545cced
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-20 16:25:44 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,15 +1,27 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  
+  // Action to add a recipe
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+
+  // Action to set multiple recipes (e.g., for initializing the store)
   setRecipes: (recipes) => set({ recipes }),
+
+  // Action to update a recipe by ID
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  // Action to delete a recipe by ID
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+  }))
 }));
 
 export default useRecipeStore;
-
-
-
-
-
-
