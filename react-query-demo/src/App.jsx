bacb89d692e2fa907592ab9cdf610167da9f1eb4
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 10:40:18 +0200

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -31,5 +31,11 @@ function App() {
     </>
   )
 }
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+<QueryClientProvider client={queryClient}>
+  <PostsComponent />
+  <ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>
 
 export default App
