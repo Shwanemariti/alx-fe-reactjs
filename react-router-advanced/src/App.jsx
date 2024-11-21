1c9c1a2cfb13549a03801d318aae86d5b0496357
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 11:02:34 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -33,28 +33,33 @@ function App() {
 }
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import Post from './components/Post';
-import ProtectedRoute from './components/ProtectedRoute';
+// src/components/Profile.jsx
 
-function App() {
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
+
+const Profile = () => {
   return (
-    <Router>
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+      
+      {/* Nested Routes */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-        <Route path="/post/:id" element={<Post />} />
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
       </Routes>
-    </Router>
+    </div>
   );
-}
+};
 
-export default App;
+export default Profile;
 
-export default App
+  
