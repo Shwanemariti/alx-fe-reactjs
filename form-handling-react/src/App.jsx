0846d89db07e81e35e6ea73f18c53e629f586b34
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 10:35:14 +0200

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -104,27 +104,6 @@ const App = () => {
             <FormikForm />
         </div>
     );
-};
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent';
-
-const queryClient = new QueryClient();
-
-function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <h1>React Query Demo</h1>
-            <PostsComponent />
-        </QueryClientProvider>
-    );
-}
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-<QueryClientProvider client={queryClient}>
-    <PostsComponent />
-    <ReactQueryDevtools initialIsOpen={false} />
-</QueryClientProvider>
 
 export default App;
 
