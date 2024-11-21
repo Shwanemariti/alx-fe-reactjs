fe396c5b0bce7ff9f4a0d8157769794eae8d42d2
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 10:52:04 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -31,5 +31,30 @@ function App() {
     </>
   )
 }
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import Post from './components/Post';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+        <Route path="/post/:id" element={<Post />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
 export default App
