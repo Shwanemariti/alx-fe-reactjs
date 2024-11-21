fb5c8a4af4497b1e37bdd62537050bb6e00ab1b5
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 09:23:32 +0200

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -105,8 +105,23 @@ const App = () => {
         </div>
     );
 };
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+
+const queryClient = new QueryClient();
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <h1>React Query Demo</h1>
+            <PostsComponent />
+        </QueryClientProvider>
+    );
+}
 
 export default App;
+
 
 
 
