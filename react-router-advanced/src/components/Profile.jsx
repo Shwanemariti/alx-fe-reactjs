82b3e6d6ec682692a77cc7954d72c851d0e2c746
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 10:51:06 +0200

    Create Profile.jsx

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <Link to="details">Profile Details</Link> | 
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+      <Outlet /> {/* This will render the nested routes */}
+    </div>
+  );
+}
+
+export default Profile;
