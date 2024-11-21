82b96d023c42b67d3b19eca750cd6610629e9834
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 10:43:39 +0200

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -9,10 +9,10 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+  const { data, isLoading, isError, error, refetch } = useQuery('posts', fetchPosts);
 
   if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <div>
