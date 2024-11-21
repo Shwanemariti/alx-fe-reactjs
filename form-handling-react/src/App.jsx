9a04b4c3953c19748e86289ad49ffb6f5d7c4107
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-21 08:43:15 +0200

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -79,8 +79,20 @@ const App = () => {
     </div>
   );
 };
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+
+const App = () => {
+    return (
+        <div>
+            <h1>User Registration</h1>
+            <RegistrationForm />
+        </div>
+    );
+};
 
 export default App;
+
 
 
 
