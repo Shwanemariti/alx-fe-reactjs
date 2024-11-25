70c4b9beef69a021bc5f6013d97496c02d55b42c
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-25 14:02:26 +0200

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     </>
   )
 }
-export default function App() {
+function App() {
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
