f65b0e621d1746ef08e4ae74bc11f07727b222a1
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-25 14:23:55 +0200

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -6,4 +6,28 @@ export default {
   },
   plugins: [],
 }
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Specify where Tailwind should look for classes
+  ],
+}
+module.exports = {
+  darkMode: 'class', // Options: 'media', 'class', or false (default)
+}
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: ['active'], // Example of extending background color variants
+    },
+  },
+}
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
