df01d44c1a4cae4eed82b0cbea132548514d92f8
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-25 13:01:05 +0200

    Update tailwind.config.js

--- tailwind-react-integration/src/tailwind.config.js
+++ tailwind-react-integration/src/tailwind.config.js
@@ -9,4 +9,15 @@ module.exports = {
   },
   plugins: [],
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
