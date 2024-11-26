75191eb9a9401adbc7a1b703562ee28d93d5445b
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-11-26 08:43:01 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const UserProfile = () => {
   return (
     <div className="bg-gray-100 flex flex-col items-center p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto rounded-lg shadow-md">
@@ -9,7 +8,6 @@ const UserProfile = () => {
         alt="User Profile"
         className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full object-cover"
       />
-
       {/* User Info */}
       <h1 className="mt-4 text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">
         John Doe
