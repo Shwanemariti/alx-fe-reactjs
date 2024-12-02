bda7bebc970b160b2a33e96ad238b2a4d1ed36c8
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-12-02 09:30:56 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -66,8 +66,26 @@ const App = () => {
     </Router>
   );
 };
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
+
 
 
 
