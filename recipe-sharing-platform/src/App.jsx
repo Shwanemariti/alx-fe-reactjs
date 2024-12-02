e83aa83372347ce5af1b9547307a67cc4a6a9aa8
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-12-02 09:01:36 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -41,5 +41,17 @@ function App() {
     </div>
   );
 }
+import React from "react";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
+    </div>
+  );
+}
+
 export default App;
+
 
