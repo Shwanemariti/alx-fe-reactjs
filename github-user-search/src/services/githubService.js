634814c05c6eeb3929684cc7708f9be4a883b2d2
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-12-10 09:35:09 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -9,4 +9,25 @@ export const fetchUserData = async (username) => {
   } catch (error) {
     throw new Error('User not found');
   }
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/search/users';
+
+export const fetchAdvancedUserData = async (criteria) => {
+  const { username, location, minRepos } = criteria;
+
+  let query = [];
+  if (username) query.push(`user:${username}`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>${minRepos}`);
+
+  const queryString = query.join('+');
+  const url = `${BASE_URL}?q=${queryString}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data.items; // Array of users
+  } catch (error) {
+    throw new Error('Failed to fetch users');
+  }
 };
