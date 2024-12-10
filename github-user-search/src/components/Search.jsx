418c53bccaec3ebde76c782f1f97d08af23e9eed
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-12-10 09:27:21 +0200

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { fetchUserData } from '../services/githubService';
 function Search({ onSearch }) {
   const [username, setUsername] = useState('');
 
