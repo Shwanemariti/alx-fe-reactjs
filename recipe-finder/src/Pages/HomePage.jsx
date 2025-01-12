28cecdd7980d360c31f86c164a81ec7ff510dde5
Shwanemariti <m.mariti@lab.clicklearning.org>
2025-01-12 19:27:00 +0200

    Update HomePage.jsx

--- recipe-finder/src/Pages/HomePage.jsx
+++ recipe-finder/src/Pages/HomePage.jsx
@@ -1,1 +1,26 @@
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import RecipeCard from "../components/RecipeCard";
+import fetchRecipes from "../api";
 
+const HomePage = ({ onRecipeSelect }) => {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearch = async (query) => {
+    const results = await fetchRecipes(query);
+    setRecipes(results);
+  };
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.idMeal} recipe={recipe} onClick={onRecipeSelect} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
