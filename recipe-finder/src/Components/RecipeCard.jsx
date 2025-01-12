e12ee3684418329dbc46594bc47f2f32786d44b2
Shwanemariti <m.mariti@lab.clicklearning.org>
2025-01-12 19:25:57 +0200

    Update RecipeCard.jsx

--- recipe-finder/src/Components/RecipeCard.jsx
+++ recipe-finder/src/Components/RecipeCard.jsx
@@ -1,1 +1,17 @@
+const RecipeCard = ({ recipe, onClick }) => (
+  <div
+    className="border rounded shadow-md p-4 cursor-pointer"
+    onClick={() => onClick(recipe)}
+  >
+    <img
+      src={recipe.strMealThumb}
+      alt={recipe.strMeal}
+      className="w-full h-40 object-cover rounded"
+    />
+    <h3 className="text-lg font-semibold mt-2">{recipe.strMeal}</h3>
+    <p>{recipe.strCategory} - {recipe.strArea}</p>
+  </div>
+);
+
+export default RecipeCard;
 
